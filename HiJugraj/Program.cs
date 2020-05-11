aba872d7026ea8c842c4761607d64b6cac958f67
jugraj singh <sjugraj4694@gmail.com>
2020-05-11 12:38:20 +1200

    Hello Jugraj Program

---
+++ HiJugraj/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HiJugraj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi Jugraj");
+            Console.ReadLine();
+        }
+    }
+}
